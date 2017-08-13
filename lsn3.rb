03fee552767a8b33731717295cde24ed7733f55b
Kamon <harrellkamon@gmail.com>
2017-08-13 14:46:16 -0600

    placing variables into strings

---
+++ lsn3.rb
@@ -0,0 +1,15 @@
+name = 'Kamon A. Harrell'
+age = 22
+height = 74 # inches
+weight = 174 # lbs
+eyes = 'Hazel'
+teeth = 'White'
+hair = 'Brown'
+
+puts "lets talk about #{name}"
+puts "hes #{height} inches tall"
+puts "hes #{weight} pounds heavy"
+puts "hes got #{eyes} eyes and #{hair} hair"
+puts "his teeth are usually #{teeth} depending on the coffee"
+
+puts "if i add #{age}, #{height}, and #{weight} I get #{age + height + weight}"
