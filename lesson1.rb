8d538580cffc84c4eced4ec18eaf7a1b0d95c6e2
Kamon <harrellkamon@gmail.com>
2017-08-13 14:26:55 -0600

    simply ruby operations

---
+++ lesson1.rb
@@ -0,0 +1,23 @@
+puts "I will now count my chickens: "
+
+puts "Hens #{25 + 30 / 6}"
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+puts "Now I will count the eggs: "
+
+puts 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+
+puts "is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What it 3 + 2? #{3 + 2}"
+puts "What is 5 -7? #{5 - 7}"
+
+puts "Oh thats why its false"
+
+puts "How about some more?"
+
+puts "is it greater? #{5 > -2}"
+puts "is it greater or equal? #{5 >= -2}"
+puts "is it less than or equal? #{5 <= -2}"
