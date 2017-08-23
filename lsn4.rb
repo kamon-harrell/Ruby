750a73c64de1a8711115c81e570deedac85152cd
Kamon <harrellkamon@gmail.com>
2017-08-23 12:31:19 -0600

    more string manipulation

---
+++ lsn4.rb
@@ -0,0 +1,21 @@
+types_of_people = 10
+x = "there are #{types_of_people} types of people"
+binary = "binary"
+do_not = "don't"
+y = "those who know #{binary} and those who #{do_not}"
+
+puts x
+puts y
+
+puts "i said #{x}"
+puts "i also said #{y}"
+
+hilarious = false
+joke_eval = "isnt that joke so funny?! #{hilarious}"
+
+puts joke_eval
+
+w = "this is the left side of..."
+e = "a string with a right side"
+
+puts w + e
